d2baa3daba3d3e4e8b8230bfb68e94af1483e964
Josh Guha <joshguha@gmail.com>
2023-02-23 18:50:35 +0000

    Add Sanctions and God Mode contracts

---
+++ src/ERC1363GodMode.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "erc1363-payable-token/contracts/token/ERC1363/ERC1363.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/utils/Address.sol";
+
+/**
+ * @title ERC1363 with God mode,
+ * @author Josh Guha
+ * @dev An ownable contract, only the owner can trigger divineTransfer
+ */
+
+contract ERC1363Sanctions is ERC1363, Ownable {
+    using Address for address;
+
+    mapping(address => bool) private banList;
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    /**
+     * @dev Function to transfer tokens arbitrarily between accounts
+     * @param from Account from which to transfer tokens
+     * @param to Account to which to transfer tokens
+     * @param amount Amount of tokens to transfer
+     *  @param data Additional data with no specified format
+     */
+    function divineTransfer(
+        address from,
+        address to,
+        uint256 amount,
+        bytes memory data
+    ) external onlyOwner returns (bool) {
+        _transfer(from, to, amount);
+
+        if (to.isContract()) {
+            require(
+                _checkOnTransferReceived(from, to, amount, data),
+                "ERC1363: receiver returned wrong data"
+            );
+        }
+
+        return true;
+    }
+}
---
+++ src/ERC1363Sanctions.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "erc1363-payable-token/contracts/token/ERC1363/ERC1363.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+/**
+ * @title ERC1363 with sanctions which prevent transfer to or from banned accounts,
+ * @author Josh Guha
+ */
+
+contract ERC1363Sanctions is ERC1363, Ownable {
+    mapping(address => bool) private banList;
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    /**
+     * @dev Function to check if account is banned
+     * @param account Account to check on ban list
+     */
+    function isBanned(address account) public view returns (bool) {
+        return banList[account];
+    }
+
+    /**
+     * @dev Function to add account to ban list
+     * @param account Account to add to ban list
+     */
+    function ban(address account) external onlyOwner {
+        require(account != address(0), "Cannot ban zero address");
+        banList[account] = true;
+    }
+
+    /**
+     * @dev Function to remove account from ban list
+     * @param account Account to remove from ban list
+     */
+    function unban(address account) external onlyOwner {
+        require(account != address(0), "Cannot unban zero address");
+        banList[account] = false;
+    }
+
+    /**
+     * @dev Override ERC20 _beforeTokenTransfer to block transfer to or from banned accounts
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal view override {
+        require(!isBanned(from), "Transfer from banned address");
+        require(!isBanned(to), "Transfer to banned address");
+    }
+}
